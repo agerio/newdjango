e2172e76d2db406c5920decc0fc66ad575ca9eb9
Jovian <ajovian440@gmail.com>
2024-08-22 15:58:25 +1000

    Change project name to sibanjir

--- sibanjir/wsgi.py
+++ sibanjir/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for api project.
+WSGI config for sibanjir project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sibanjir.settings')
 
 application = get_wsgi_application()
 app = application
